3f454733603763ba72969bc9ef7af9b62aed0f0d
Andrii_Tetiushev <andrii_tetiushev@epam.com>
2022-12-05 13:22:54 +0200

    ability to pass request headers

--- vars/doGet.groovy
+++ vars/doGet.groovy
@@ -1,9 +1,9 @@
 import com.example.http.HttpClient
 import com.example.http.HttpResponse
 
-def call(String url) {
+def call(String url, Map headers = [:]) {
     HttpClient client = new HttpClient();
-    HttpResponse response = client.doGetHttpRequest(url)
+    HttpResponse response = client.doGetHttpRequest(url, headers)
     echo "response status " + response.getStatusCode()
     echo "response " + response.body
 }
