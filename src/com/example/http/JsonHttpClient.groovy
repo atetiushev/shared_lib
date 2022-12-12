89c03cb3c71915aafa3af2d2ba7c52ce5ba18c33
Andrii_Tetiushev <andrii_tetiushev@epam.com>
2022-12-12 13:59:43 +0200

    extend functionality

--- src/com/example/http/JsonHttpClient.groovy
+++ src/com/example/http/JsonHttpClient.groovy
@@ -1,11 +1,27 @@
 package com.example.http
 
 class JsonHttpClient extends HttpClient {
+
+    private String userName
+    private String password
+
     JsonHttpClient() {
         this(null)
     }
+
     JsonHttpClient(script) {
         super(script)
         this.responseBodyConverter = new JsonConverter();
     }
+
+    HttpClient withUserName(String userName) {
+        this.@userName = userName
+        return this
+    }
+
+    HttpClient withPassword(String password) {
+        this.@password = password
+        return this
+    }
+
 }
